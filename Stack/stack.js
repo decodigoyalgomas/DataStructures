a8382970df788dc99105f29ab3cc4fc765d48e0a
Pedro Baumann <ondoheer@gmail.com>
2015-03-01 22:38:14 -0500

    reverse string function

--- Stack/stack.js
+++ Stack/stack.js
@@ -4,8 +4,24 @@ var Stack = function(){
 };
 
 Stack.prototype.size = function(){return this.items.length}
-Stack.prototype.isEmpty = function(){return this.items == []}
+Stack.prototype.isEmpty = function(){return this.size() == 0 }
 Stack.prototype.push = function(item){this.items.push(item)}
-Stack.prototype.pop = function(item){ this.items.pop()}
+Stack.prototype.pop = function(item){ return this.items.pop()}
 Stack.prototype.peek = function(){return this.items[this.items.length-1]}
 Stack.prototype.size = function(){return this.items.length}
+
+
+
+function revstring(word){
+	s = new Stack();
+	
+	for(var letter in word){
+		s.push(word[letter]);
+	}
+	var reverse =""	;
+	
+	while( !s.isEmpty() ){
+		reverse += s.pop();
+	}
+	return reverse;
+}
